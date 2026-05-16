afee7f9a75488d9dbd0c80f3278f3314409c5b18
kylegwlawrence <kylegwlawrence@gmail.com>
2026-05-16 13:07:02 -0700

    Add ENWIKI_RAG_DB path to start_proxy.sh

--- start_proxy.sh
+++ start_proxy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 export ENWIKI_DB=/home/kyle/Documents/projects/local_wikipedia/dumps/enwiki.db
+export ENWIKI_RAG_DB=/home/kyle/Documents/projects/local_wikipedia/dumps/enwiki_rag.db
 exec uvicorn proxy_server:app --host 0.0.0.0 --port 8000
